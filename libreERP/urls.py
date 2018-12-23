863adac83ad79819feb8175a41bd3ecf7ea3b9ea
pradeep <pradeep@cioc.co.in>
2018-12-23 17:18:43 +0530

    minor

--- libreERP/urls.py
+++ libreERP/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     url(r'^token', tokenAuthentication , name ='tokenAuthentication'),
     url(r'^logout/', logoutView , name ='logout'),
     url(r'^api-auth/', include('rest_framework.urls', namespace ='rest_framework')),
-    # url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include('allauth.urls')),
     url(r'^robots\.txt', include('robots.urls')),
     url(r'^generateOTP', generateOTP, name="generateOTP"),
     url(r'^documents', documentView , name ='document'),
