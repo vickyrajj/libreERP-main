181fa5184440be52c3fbf67519a5292478c95ca4
cioc <dev.vigneshraj.s@gmail.com>
2018-12-25 18:53:07 +0530

    abc

--- marketing/static/js/app.marketing.js
+++ marketing/static/js/app.marketing.js
@@ -52,6 +52,147 @@ app.config(function($stateProvider) {
 
 app.controller("businessManagement.marketing.default", function($scope, $state, $users, $stateParams, $http, Flash) {
 
+  new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''],
+      datasets: [{
+        data: [860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478, 860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478,860, -114, 1060, -306, 107, -888, -133, 1000,],
+        label: "",
+        borderColor: "#3e95cd",
+        fill: false,
+        lineTension: 0,
+      }]
+    },
+    options: {
+      legend:{
+        display:false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: '',
+        legend: false,
+        lable: false,
+        showLine: false,
+      }
+    }
+  });
+  new Chart(document.getElementById("line-chart1"), {
+    type: 'line',
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''],
+      datasets: [{
+        data: [860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478, 860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478,860, -114, 1060, -306, 107, -888, -133, 1000,],
+        label: "",
+        borderColor: "#3e95cd",
+        fill: false,
+        lineTension: 0,
+      }]
+    },
+    options: {
+      legend:{
+        display:false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: '',
+        legend: false,
+        lable: false,
+        showLine: false,
+      }
+    }
+  });
+  new Chart(document.getElementById("line-chart2"), {
+    type: 'line',
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''],
+      datasets: [{
+        data: [860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478, 860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478,860, -114, 1060, -306, 107, -888, -133, 1000,],
+        label: "",
+        borderColor: "#3e95cd",
+        fill: false,
+        lineTension: 0,
+      }]
+    },
+    options: {
+      legend:{
+        display:false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: '',
+        legend: false,
+        lable: false,
+        showLine: false,
+      }
+    }
+  });
 
 
 })
--- virtualWorkforce/static/js/app.virtualWorkforce.js
+++ virtualWorkforce/static/js/app.virtualWorkforce.js
@@ -1,66 +1,226 @@
-app.config(function($stateProvider){
+app.config(function($stateProvider) {
 
   $stateProvider
-  .state('businessManagement.virtualWorkforce', {
-    url: "/virtualWorkforce",
-    views: {
-       "": {
+    .state('businessManagement.virtualWorkforce', {
+      url: "/virtualWorkforce",
+      views: {
+        "": {
           templateUrl: '/static/ngTemplates/genericAppBase.html',
-       },
-       "menu@businessManagement.virtualWorkforce": {
+        },
+        "menu@businessManagement.virtualWorkforce": {
           templateUrl: '/static/ngTemplates/genericMenu.html',
-          controller : 'controller.generic.menu',
+          controller: 'controller.generic.menu',
         },
         "@businessManagement.virtualWorkforce": {
           templateUrl: '/static/ngTemplates/app.virtualWorkforce.default.html',
+          controller: 'businessManagement.virtualWorkforce.default',
         }
-    }
-  })
-  .state('businessManagement.virtualWorkforce.controlRoom', {
-    url: "/controlRoom",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.controlRoom.html',
-    controller: 'businessManagement.virtualWorkforce.controlRoom'
-  })
-  .state('businessManagement.virtualWorkforce.configure', {
-    url: "/configure",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.configure.html',
-    controller: 'businessManagement.virtualWorkforce.configure'
-  })
-  .state('businessManagement.virtualWorkforce.environments', {
-    url: "/environments",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.environments.html',
-    controller: 'businessManagement.virtualWorkforce.environments'
-  })
-  .state('businessManagement.virtualWorkforce.processes', {
-    url: "/processes",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.processes.html',
-    controller: 'businessManagement.virtualWorkforce.processes'
-  })
-  .state('businessManagement.virtualWorkforce.queues', {
-    url: "/queues",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.queues.html',
-    controller: 'businessManagement.virtualWorkforce.queues'
-  })
-  .state('businessManagement.virtualWorkforce.releases', {
-    url: "/releases",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.releases.html',
-    controller: 'businessManagement.virtualWorkforce.releases'
-  })
-  .state('businessManagement.virtualWorkforce.reports', {
-    url: "/reports",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.reports.html',
-    controller: 'businessManagement.virtualWorkforce.reports'
-  })
-  .state('businessManagement.virtualWorkforce.schedules', {
-    url: "/schedules",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.schedules.html',
-    controller: 'businessManagement.virtualWorkforce.schedules'
-  })
-  .state('businessManagement.virtualWorkforce.robots', {
-    url: "/robots",
-    templateUrl: '/static/ngTemplates/app.virtualWorkforce.robots.html',
-    controller: 'businessManagement.virtualWorkforce.robots'
-  })
+      }
+    })
+    .state('businessManagement.virtualWorkforce.controlRoom', {
+      url: "/controlRoom",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.controlRoom.html',
+      controller: 'businessManagement.virtualWorkforce.controlRoom'
+    })
+    .state('businessManagement.virtualWorkforce.configure', {
+      url: "/configure",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.configure.html',
+      controller: 'businessManagement.virtualWorkforce.configure'
+    })
+    .state('businessManagement.virtualWorkforce.environments', {
+      url: "/environments",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.environments.html',
+      controller: 'businessManagement.virtualWorkforce.environments'
+    })
+    .state('businessManagement.virtualWorkforce.processes', {
+      url: "/processes",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.processes.html',
+      controller: 'businessManagement.virtualWorkforce.processes'
+    })
+    .state('businessManagement.virtualWorkforce.queues', {
+      url: "/queues",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.queues.html',
+      controller: 'businessManagement.virtualWorkforce.queues'
+    })
+    .state('businessManagement.virtualWorkforce.releases', {
+      url: "/releases",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.releases.html',
+      controller: 'businessManagement.virtualWorkforce.releases'
+    })
+    .state('businessManagement.virtualWorkforce.reports', {
+      url: "/reports",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.reports.html',
+      controller: 'businessManagement.virtualWorkforce.reports'
+    })
+    .state('businessManagement.virtualWorkforce.schedules', {
+      url: "/schedules",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.schedules.html',
+      controller: 'businessManagement.virtualWorkforce.schedules'
+    })
+    .state('businessManagement.virtualWorkforce.robots', {
+      url: "/robots",
+      templateUrl: '/static/ngTemplates/app.virtualWorkforce.robots.html',
+      controller: 'businessManagement.virtualWorkforce.robots'
+    })
 
 
 });
+
+
+
+app.controller('businessManagement.virtualWorkforce.default', function($scope, $http, $aside, $state, Flash, $users, $filter, $permissions) {
+  // settings main page controller
+  $scope.labels = ["Download Sales", "In-Store Sales"];
+  $scope.data = [70, 30];
+  $scope.labels1 = ["Download Sales", "In-Store Sales"];
+  $scope.data1 = [30, 70];
+
+  $scope.barlabels = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'];
+
+  $scope.bardata = [
+    [28, 48, 40, 19, 86, 27, 90,34,55,21,32,32,32,45,56,45]
+  ];
+
+  new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''],
+      datasets: [{
+        data: [860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478, 860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478,860, -114, 1060, -306, 107, -888, -133, 1000,],
+        label: "",
+        borderColor: "#1DA381",
+        fill: false,
+        lineTension: 0,
+      }]
+    },
+    options: {
+      legend:{
+        display:false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: '',
+        legend: false,
+        lable: false,
+        showLine: false,
+      }
+    }
+  });
+  new Chart(document.getElementById("line-chart1"), {
+    type: 'line',
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''],
+      datasets: [{
+        data: [860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478, 860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478,860, -114, 1060, -306, 107, -888, -133, 1000,],
+        label: "",
+        borderColor: "#1DA381",
+        fill: false,
+        lineTension: 0,
+      }]
+    },
+    options: {
+      legend:{
+        display:false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: '',
+        legend: false,
+        lable: false,
+        showLine: false,
+      }
+    }
+  });
+  new Chart(document.getElementById("line-chart2"), {
+    type: 'line',
+    data: {
+      labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '','', '', '', '', '', '', '', '', '', ''],
+      datasets: [{
+        data: [860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478, 860, -114, 1060, -306, 107, -888, -133, 1000, -783, 2478,860, -114, 1060, -306, 107, -888, -133, 1000,],
+        label: "",
+        borderColor: "#1DA381",
+        fill: false,
+        lineTension: 0,
+      }]
+    },
+    options: {
+      legend:{
+        display:false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }],
+        yAxes: [{
+          ticks: {
+            display: false
+          },
+          gridLines: {
+            color: "rgba(0, 0, 0, 0)",
+            drawBorder: false,
+            display: false,
+          }
+        }]
+      },
+      title: {
+        display: false,
+        text: '',
+        legend: false,
+        lable: false,
+        showLine: false,
+      }
+    }
+  });
+
+})
