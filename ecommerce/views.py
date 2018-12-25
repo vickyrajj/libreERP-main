81fd8badc7a135a062fbfcc033399019965e7372
ankitakadam16 <ksankita16@gmail.com>
2018-12-25 11:29:42 +0530

    invoice

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1743,31 +1743,31 @@ def genInvoice(response, contract, request):
     pdf_doc.build(story,onFirstPage=addPageNumber, onLaterPages=addPageNumber, canvasmaker=PageNumCanvas)
 
 
-def link_callback(uri, rel):
-    """
-    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
-    resources
-    """
-    # use short variable names
-    sUrl = settings.STATIC_URL      # Typically /static/
-    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
-    mUrl = settings.MEDIA_URL       # Typically /static/media/
-    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
-
-    # convert URIs to absolute system paths
-    if uri.startswith(mUrl):
-        path = os.path.join(mRoot, uri.replace(mUrl, ""))
-    elif uri.startswith(sUrl):
-        path = os.path.join(sRoot, uri.replace(sUrl, ""))
-    else:
-        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
-
-    # make sure that file exists
-    if not os.path.isfile(path):
-            raise Exception(
-                'media URI must start with %s or %s' % (sUrl, mUrl)
-            )
-    return path
+# def link_callback(uri, rel):
+#     """
+#     Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+#     resources
+#     """
+#     # use short variable names
+#     sUrl = settings.STATIC_URL      # Typically /static/
+#     sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
+#     mUrl = settings.MEDIA_URL       # Typically /static/media/
+#     mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
+#
+#     # convert URIs to absolute system paths
+#     if uri.startswith(mUrl):
+#         path = os.path.join(mRoot, uri.replace(mUrl, ""))
+#     elif uri.startswith(sUrl):
+#         path = os.path.join(sRoot, uri.replace(sUrl, ""))
+#     else:
+#         return uri  # handle absolute uri (ie: http://some.tld/foo.png)
+#
+#     # make sure that file exists
+#     if not os.path.isfile(path):
+#             raise Exception(
+#                 'media URI must start with %s or %s' % (sUrl, mUrl)
+#             )
+#     return path
 
 # def genBNIInvoice(response,o, request):
 #     template_path = 'invoice.html'
--- libreERP/settings.py
+++ libreERP/settings.py
@@ -75,7 +75,7 @@ USE_CDN = False # when turned on the application will use the cndjs.com and othe
 # Application definition
 BRAND_NAME = 'sterlingselect.com'
 SERVICE_NAME = 'Inventory, Sales and Ecommerce'
-BRAND_LOGO = '/static/images/company_icon.svg'
+BRAND_LOGO = '/static/images/bni-2.svg'
 BRAND_LOGO_INVERT = '/static/images/mono_icon_inverted.svg'
 SMART_REGISTRATION = True
 AUTO_ACTIVE_ON_REGISTER = False
