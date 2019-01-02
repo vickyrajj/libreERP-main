38050fa8b111e9d381a5878ee23559ed352de1fd
pradeep <pradeep@cioc.co.in>
2019-01-02 15:15:57 +0530

    minor

--- ecommerce/fedex_config.py
+++ ecommerce/fedex_config.py
@@ -14,9 +14,9 @@ from django.conf import settings as globalSettings
 
 
 # Change these values to match your testing account/meter number.
-CONFIG_OBJ = FedexConfig(key= 'TAQDL2KcVX0ijQOQ' ,
-                         password= '7UAEID6HfVBLfmiHnFH4EhRfH',
-                         account_number= '510087500' ,
-                         meter_number= '119097717',
+CONFIG_OBJ = FedexConfig(key= 'abOhatnikLWCa8Hj' ,
+                         password= 'Yk1agUkfxS1P3ABNemVGYJFOB',
+                         account_number= '870648022' ,
+                         meter_number= '113921415',
                          # freight_account_number='510087020',
-use_test_server=True)
+use_test_server=globalSettings.FEDEX_TEST_MODE)
