12b3a815b5f799c8dffee495afa4baa193d2ef71
himanshu <himanshu@cioc.co.in>
2019-01-26 20:59:05 +0530

    pushing

--- homepage/static/js/app.homepage.main.js
+++ homepage/static/js/app.homepage.main.js
@@ -1,5 +1,16 @@
 var app = angular.module('app', ['ui.router', 'ui.bootstrap', 'angular-owl-carousel-2', 'ui.bootstrap.datetimepicker', 'flash', 'ngAside']);
 // $scope, $state, $users, $stateParams, $http, $timeout, $uibModal , $sce,$rootScope
+
+
+app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+
+  $urlRouterProvider.otherwise('/');
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $httpProvider.defaults.withCredentials = true;
+
+})
 app.controller('main', function($scope, $http, $sce, $interval, $uibModal) {
   console.log("main loded");
   $scope.crmBannerID = 1;
@@ -101,7 +112,78 @@ app.controller('main', function($scope, $http, $sce, $interval, $uibModal) {
       templateUrl: '/static/ngTemplates/app.homepage.signin.html',
       size: 'lg',
       backdrop: false,
-      controller: function($scope, $uibModalInstance) {
+      controller: function($scope, $uibModalInstance, $timeout, Flash) {
+        $scope.form = {
+          number: '',
+          otp: '',
+          otpmode: false,
+          errorMessage: '',
+          errorType: 'default'
+        }
+        $scope.loginFunction = function() {
+          console.log($scope.form);
+          if (!$scope.form.otpmode) {
+            if ($scope.form.number.length != 10) {
+              $scope.form.errorMessage = 'Enter Valid Mobile Number'
+              $scope.form.errorType = 'danger'
+              return
+            } else {
+              $scope.form.errorMessage = ''
+              $scope.form.errorType = 'default'
+            }
+            $http({
+              method: 'POST',
+              url: '/generateOTP',
+              data: {
+                'id': $scope.form.number
+              }
+            }).
+            then(function(response) {
+              console.log(response.data);
+              $scope.form.otpmode = true
+            }, function(err) {
+              if (err.status == 404) {
+                $http({
+                  method: 'POST',
+                  url: '/api/homepage/registration/',
+                  data: {mobile:$scope.form.number}
+                }).
+                then(function(response) {
+                  console.log(response.data);
+                  $scope.form.errorMessage = 'You Have No Account , We Are Creating New Account For You'
+                  $scope.form.errorType = 'warning'
+                  $scope.form.otpmode = true
+                  $scope.form.token = response.data.token
+                }).catch(function(err) {
+                  $scope.form.errorMessage = 'Invalid Data'
+                  $scope.form.errorType = 'danger'
+                })
+              } else if (err.status == 400) {
+                $scope.form.errorMessage = 'No Account'
+                $scope.form.errorType = 'danger'
+              } else if (err.status == 500) {
+                $scope.form.errorMessage = 'Error While Sending OTP'
+                $scope.form.errorType = 'danger'
+              }
+            });
+          } else {
+            console.log('enter otp mode');
+            if ($scope.form.token!=undefined) {
+              var toSend = {mobile:$scope.form.number,mobileOTP:$scope.form.otp,token:$scope.form.token}
+            }else {
+              var toSend = {mobile:$scope.form.number,mobileOTP:$scope.form.otp}
+            }
+            $http({
+              method: 'POST',
+              url: '/registerLite',
+              data: toSend
+            }).
+            then(function(response) {
+              console.log('Registered');
+              window.location.href = "/";
+            })
+          }
+        }
         $scope.close = function() {
           $uibModalInstance.dismiss('cancel');
         }
