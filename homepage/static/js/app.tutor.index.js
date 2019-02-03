602a693bb8e3aef52402b724ca7231d87b72438b
pradeep <pradeep@cioc.co.in>
2019-02-03 11:38:17 +0530

    minor

--- homepage/static/js/app.tutor.index.js
+++ homepage/static/js/app.tutor.index.js
@@ -55,4 +55,28 @@ app.controller('main', function($scope, $http, $interval,$uibModal,$rootScope) {
       $rootScope.$broadcast('opensignInPopup', loggedIn);
 
   }
+
+  $scope.viewProfile = function(who) {
+
+    $uibModal.open({
+      templateUrl: '/static/ngTemplates/app.tutor.profile.html',
+      size: 'md',
+      backdrop: true,
+      resolve : {
+        who : function() {
+          return who
+        }
+      },
+      controller: function($scope, $http , who) {
+        $scope.who = who;
+      }
+    })
+
+  }
+
+  $scope.becomeATeacher = function() {
+    window.location.href = '/career';
+  }
+
+
 })
